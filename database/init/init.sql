cffbc346dbf94930bf9e594300f249cd924bad19
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2023-07-09 17:04:56 +0200

    alter sql init

--- database/init/init.sql
+++ database/init/init.sql
@@ -3,29 +3,29 @@ CREATE DATABASE IF NOT EXISTS `db`
 
 USE `db`;
 
--- Create relations
+-- Create relations ------------------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS `record` (
-  `id`        BIGINT NOT NULL AUTO_INCREMENT,
+  `recId`     BIGINT NOT NULL AUTO_INCREMENT,
   `url`       VARCHAR(2048) NOT NULL,
   `regexp`    VARCHAR(1024) NOT NULL,
   `period`    INT NOT NULL, -- minutes!
   `label`     VARCHAR(1024) NOT NULL,
   `active`    TINYINT NOT NULL,
   `tags`      JSON NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`recId`)
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `execution` (
-  `id`        BIGINT NOT NULL AUTO_INCREMENT,
-  `recordId`  BIGINT NOT NULL,
-  `status`    ENUM('CREATED', 'QUEUED', 'CRAWLING', 'FINISHED', 'FAILED')
+  `exeId`     BIGINT NOT NULL AUTO_INCREMENT,
+  `recId`     BIGINT NOT NULL,
+  `status`    ENUM('CREATED', 'PLANNED', 'WAITING', 'CRAWLING', 'FINISHED')
               NOT NULL DEFAULT 'CREATED',
   `startTime` DATETIME NOT NULL,
   `endTime`   DATETIME,
   `linkCount` INT NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`recordId`) REFERENCES `record`(`id`)
+  PRIMARY KEY (`exeId`),
+  FOREIGN KEY (`recId`) REFERENCES `record`(`recId`)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
@@ -40,7 +40,7 @@ DELIMITER $
 CREATE PROCEDURE `getAllRecords` ()
 BEGIN
   SELECT
-    `r`.`id`        AS `id`,
+    `r`.`recId`     AS `recId`,
     `r`.`url`       AS `url`,
     `r`.`regexp`    AS `regexp`,
     `r`.`period`    AS `period`,
@@ -52,59 +52,75 @@ BEGIN
     `e`.`startTime` AS `lastExecStartTime`
   FROM `record` AS `r` LEFT JOIN (
     SELECT
-      -- e0.`id`,
-      `e0`.`recordId`,
+      -- e0.`exeId`,
+      `e0`.`recId`,
       `e0`.`status`,
       `e0`.`startTime`,
       `e0`.`endTime`
     FROM `execution` AS `e0` LEFT JOIN `execution` AS `e1`
-      ON `e0`.`recordId` = `e1`.`recordId`
-        AND (`e0`.`startTime` < `e1`.`startTime` OR (`e0`.`startTime` = `e1`.`startTime` AND `e0`.`id` < `e1`.`id`))
+      ON `e0`.`recId` = `e1`.`recId`
+        AND (`e0`.`startTime` < `e1`.`startTime` OR (`e0`.`startTime` = `e1`.`startTime` AND `e0`.`exeId` < `e1`.`exeId`))
     WHERE `e1`.startTime IS NULL
   ) AS `e`
-  ON `r`.`id` = `e`.`recordId`;
+  ON `r`.`recId` = `e`.`recId`;
 END$
 
 CREATE PROCEDURE `createRecord` (
-  IN  `p_url`     VARCHAR(2048),
-  IN  `p_regexp`  VARCHAR(1024),
-  IN  `p_period`  INT,
-  IN  `p_label`   VARCHAR(1024),
-  IN  `p_active`  TINYINT,
-  IN  `p_tags`    JSON
+  IN  `i_url`       VARCHAR(2048),
+  IN  `i_regexp`    VARCHAR(1024),
+  IN  `i_period`    INT,
+  IN  `i_label`     VARCHAR(1024),
+  IN  `i_active`    TINYINT,
+  IN  `i_tags`      JSON,
+  IN  `i_startTime` DATETIME,
+  OUT `o_recId`     BIGINT,
+  OUT `o_exeId`     BIGINT
 )
 BEGIN
-  INSERT INTO `record` (`url`, `regexp`, `period`, `label`, `active`, `tags`) VALUES
-    (`p_url`, `p_regexp`, `p_period`, `p_label`, `p_active`, `p_tags`);
-END$
+  DECLARE EXIT HANDLER FOR SQLEXCEPTION
+  BEGIN
+    ROLLBACK;
+    RESIGNAL;
+  END;
+  START TRANSACTION;
+    INSERT INTO `record` (`url`, `regexp`, `period`, `label`, `active`, `tags`) VALUES
+      (`i_url`, `i_regexp`, `i_period`, `i_label`, `i_active`, `i_tags`);
+    SELECT LAST_INSERT_ID () INTO `o_recId`;
+    IF `i_active` > 0 THEN
+      INSERT INTO `execution` (`recId`, `startTime`) VALUES
+        (`o_recId`, `i_startTime`);
+      SELECT LAST_INSERT_ID () INTO `o_exeId`;
+    END IF;
+  COMMIT;
+END$$
 
 CREATE PROCEDURE `updateRecord` (
-  IN  `p_id`      BIGINT,
-  IN  `p_url`     VARCHAR(2048),
-  IN  `p_regexp`  VARCHAR(1024),
-  IN  `p_period`  INT,
-  IN  `p_label`   VARCHAR(1024),
-  IN  `p_active`  TINYINT,
-  IN  `p_tags`    JSON
+  IN  `i_recId`   BIGINT,
+  IN  `i_url`     VARCHAR(2048),
+  IN  `i_regexp`  VARCHAR(1024),
+  IN  `i_period`  INT,
+  IN  `i_label`   VARCHAR(1024),
+  IN  `i_active`  TINYINT,
+  IN  `i_tags`    JSON
 )
 BEGIN
   UPDATE `record`
   SET
-    `url`    = `p_url`,
-    `regexp` = `p_regexp`,
-    `period` = `p_period`,
-    `label`  = `p_label`,
-    `active` = `p_active`,
-    `tags`   = `p_tags`
-  WHERE `id` = `p_id`;
-END$
+    `url`    = `i_url`,
+    `regexp` = `i_regexp`,
+    `period` = `i_period`,
+    `label`  = `i_label`,
+    `active` = `i_active`,
+    `tags`   = `i_tags`
+  WHERE `recId` = `i_recId`;
+END$$
 
 CREATE PROCEDURE `deleteRecord` (
-  IN `p_id`       BIGINT
+  IN `i_recId`    BIGINT
 )
 BEGIN
-  DELETE FROM `record` WHERE `id` = `p_id`;
-END$
+  DELETE FROM `record` WHERE `recId` = `i_recId`;
+END$$
 
 DELIMITER ;
 
@@ -115,7 +131,7 @@ INSERT INTO `record` (`url`, `regexp`, `period`, `label`, `active`, `tags`) VALU
   ('http://www.example2.com', '[0-9]+', 2, 'Example web 2', 1, JSON_ARRAY('b', 'c')),
   ('http://www.example3.com', '[a-z]*', 3, 'Example web 3', 1, JSON_ARRAY('c', 'd'));
 
-INSERT INTO `execution` (`recordId`, `startTime`) VALUES
+INSERT INTO `execution` (`recId`, `startTime`) VALUES
   (1, '2020-01-01 00:00:00'),
   (1, '2020-01-01 01:00:00'),
   (1, '2020-01-01 01:00:00'),

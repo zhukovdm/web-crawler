27dbe6ec7e1bebe430c395326262bbffb5aa7113
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2023-07-11 17:59:18 +0200

    reflect requests in mysql model

--- backend-openapi/src/services/mysql-model.ts
+++ backend-openapi/src/services/mysql-model.ts
@@ -2,12 +2,21 @@ import {
   Pool,
   createPool
 } from "mysql";
+import { MYSQL_CONFIG } from "./config";
 import {
+  ExecutionFullType,
+  ExecutionStatus,
+  NodeBaseType,
+  NodeIdType,
   RecordBaseType,
   RecordFullType,
   RecordIdType
 } from "../domain/types";
-import { IRecordModel } from "../domain/interfaces";
+import {
+  ICrawlerModel,
+  IExecutionModel,
+  IRecordModel
+} from "../domain/interfaces";
 
 type MySqlPackType = {
   host: string;
@@ -27,16 +36,34 @@ SELECT @recId AS recId, @exeId AS exeId;
 `;
 
 const UPDATE_RECORD_QUERY: string = `
-CALL updateRecord (?, ?, ?, ?, ?, ?, ?);
-SELECT ROW_COUNT() AS count;
+CALL updateRecord (?, ?, ?, ?, ?, ?, ?, ?, @count, @exeId);
+SELECT @count AS count, @exeId AS exeId;
 `;
 
 const DELETE_RECORD_QUERY: string = `
-CALL deleteRecord (?);
-SELECT ROW_COUNT() AS count;
+CALL deleteRecord (?, @count);
+SELECT @count AS count;
 `;
 
-export class MySqlModel implements IRecordModel {
+const GET_ALL_EXECUTIONS_QUERY: string = `
+CALL getAllExecutions ();
+`;
+
+const CREATE_EXECUTION_QUERY: string = `
+CALL createExecution (?, ?, @exeId);
+SELECT @exeId AS exeId;
+`;
+
+const CREATE_NODE_QUERY: string = `
+CALL createNode (?, ?, ?, ?, @nodId);
+SELECT @nodId AS nodId;
+`;
+
+const CREATE_LINK_QUERY: string = `
+CALL createLink (?, ?);
+`;
+
+export class MySqlModel implements IRecordModel, IExecutionModel, ICrawlerModel {
 
   /**
    * Connection pool.
@@ -51,34 +78,22 @@ export class MySqlModel implements IRecordModel {
   private constructor(p: MySqlPackType) {
     this.pool = createPool({
       ...p,
+      dateStrings: true,
       multipleStatements: true,
       connectionLimit: MySqlModel.CONNECTION_LIMIT,
     });
   }
 
-  public static getInstance(env: NodeJS.ProcessEnv) {
-    const {
-      MYSQL_HOST,
-      MYSQL_PORT,
-      MYSQL_USER,
-      MYSQL_DATABASE,
-      MYSQL_PASSWORD
-    } = env;
-    return new MySqlModel({
-      host: MYSQL_HOST,
-      port: parseInt(MYSQL_PORT),
-      user: MYSQL_USER,
-      database: MYSQL_DATABASE,
-      password: MYSQL_PASSWORD,
-    });
+  public static getInstance(): IRecordModel & IExecutionModel {
+    return new MySqlModel(MYSQL_CONFIG);
   }
 
   private static unpackRecordBase(r: RecordBaseType): [string, string, number, string, number, string] {
-    return [r.url, r.regexp, r.period, r.label, r.active, JSON.stringify(r.tags)];
+    return [r.url, r.regexp, r.period, r.label, r.active ? 1 : 0, JSON.stringify(r.tags)];
   }
 
   /**
-   * Get current time in YYYY-MM-DD HH:MM:SS.
+   * Current time in `YYYY-MM-DD HH:MM:SS` format.
    */
   private static getCurrentTime(): string {
     const d = new Date();
@@ -88,46 +103,81 @@ export class MySqlModel implements IRecordModel {
   public async getAllRecords(): Promise<RecordFullType[]> {
     return new Promise((res, rej) => {
       this.pool.query(GET_ALL_RECORDS_QUERY, [], (err, results) => {
-        if (err) { rej(err); }
-        else {
-          res(results[0].map((row: any) => ({ ...row, tags: JSON.parse(row.tags) })));
-        }
+        (err)
+          ? rej(err)
+          : res(results[0].map((row: any) => ({ ...row, tags: JSON.parse(row.tags), active: row.active === 1 })));
       });
     });
   }
 
-  public async createRecord(r: RecordBaseType): Promise<RecordIdType & { exeId: number }> {
+  public async createRecord(r: RecordBaseType): Promise<{ recId: number, exeId: number | null }> {
     return new Promise((res, rej) => {
       this.pool.query(CREATE_RECORD_QUERY, [
         ...MySqlModel.unpackRecordBase(r), MySqlModel.getCurrentTime()
       ], (err, results) => {
-        if (err) { rej(err); }
-        else {
-          const { recId, exeId } = results[1][0];
-          res({ recId: recId, exeId: exeId });
-        }
+        (err)
+          ? rej(err)
+          : res({ ...results[1][0] });
       });
     });
   }
 
-  public async updateRecord(r: RecordFullType): Promise<number> {
+  public async updateRecord(r: RecordFullType): Promise<{ updated: boolean, exeId: number | null }> {
     return new Promise((res, rej) => {
       this.pool.query(UPDATE_RECORD_QUERY, [r.recId, ...MySqlModel.unpackRecordBase(r)], (err, results) => {
-        if (err) { rej(err); }
-        else {
-          res(results[1][0].count);
-        }
+        (err)
+          ? rej(err)
+          : res({ ...results[1][0], updated: results[1][0].count > 0 });
       });
     });
   }
 
-  public async deleteRecord(r: RecordIdType): Promise<number> {
+  public async deleteRecord(r: RecordIdType): Promise<{ deleted: boolean }> {
     return new Promise((res, rej) => {
       this.pool.query(DELETE_RECORD_QUERY, [r.recId], (err, results) => {
-        if (err) { rej(err); }
-        else {
-          res(results[1][0].count);
-        }
+        (err)
+          ? rej(err)
+          : res({ deleted: results[1][0].count > 0 });
+      });
+    });
+  }
+
+  public async getAllExecutions(): Promise<ExecutionFullType> {
+    return new Promise((res, rej) => {
+      this.pool.query(GET_ALL_EXECUTIONS_QUERY, [], (err, results) => {
+        (err)
+          ? rej(err)
+          : res(results[0]);
+      });
+    });
+  }
+
+  public async createExecution(recId: number, status: ExecutionStatus): Promise<{ exeId: number }> {
+    return new Promise((res, rej) => {
+      this.pool.query(CREATE_EXECUTION_QUERY, [recId, status, MySqlModel.getCurrentTime()], (err, results) => {
+        (err)
+          ? rej(err)
+          : res({ ...results[0][1] });
+      });
+    });
+  }
+
+  public async createNode(n: NodeBaseType): Promise<NodeIdType> {
+    return new Promise((res, rej) => {
+      this.pool.query(CREATE_NODE_QUERY, [n.exeId, n.url, n.title, n.crawlTime], (err, results) => {
+        (err)
+          ? rej(err)
+          : res({ ...results[0][1] });
+      });
+    });
+  }
+
+  public async createLink(nodFr: number, nodTo: number): Promise<void> {
+    return new Promise((res, rej) => {
+      this.pool.query(CREATE_LINK_QUERY, [nodFr, nodTo], (err, _) => {
+        (err)
+          ? rej(err)
+          : res();
       });
     });
   }

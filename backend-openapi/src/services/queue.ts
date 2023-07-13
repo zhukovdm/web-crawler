3054f09f4fc59525bf2a1aee2440dd92543c5a44
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2023-07-13 16:00:37 +0200

    add linked list queue

--- backend-openapi/src/services/queue.ts
+++ backend-openapi/src/services/queue.ts
@@ -1,12 +1,64 @@
+/**
+ * Use for small collections.
+ */
 export class ArrayQueue<T> {
 
-  private readonly queue: T[] = [];
+  private readonly arr: T[] = [];
 
   constructor() { }
 
-  public prepend(item: T): void { this.queue.unshift(item); }
+  public prepend(item: T): void { this.arr.unshift(item); }
 
-  public enqueue(item: T): void { this.queue.push(item); }
+  public enqueue(item: T): void { this.arr.push(item); }
 
-  public dequeue(): T | undefined { return this.queue.shift(); }
+  public dequeue(): T | undefined { return this.arr.shift(); }
+
+  public empty(): boolean { return this.arr.length === 0; }
+}
+
+type LinkedListItem<T> = {
+  item: T;
+  next: LinkedListItem<T> | undefined;
+}
+
+/**
+ * Use for potentially large collections.
+ */
+export class LinkedListQueue<T> {
+
+  private head: LinkedListItem<T> | undefined = undefined;
+  private tail: LinkedListItem<T> | undefined = undefined;
+  private count: number = 0;
+
+  public prepend(item: T): void {
+    const llitem = { item: item, next: this.head };
+
+    this.head = llitem;
+    this.tail = (!this.tail) ? llitem : this.tail;
+    
+    ++this.count;
+  }
+
+  public enqueue(item: T): void {
+    const llitem = { item: item, next: undefined };
+
+    this.head = this.head ?? llitem;
+
+    if (this.tail) { this.tail.next = llitem; }
+    this.tail = llitem;
+
+    ++this.count;
+  }
+
+  public dequeue(): T | undefined {
+    const llitem = this.head;
+    this.head = this.head?.next;
+
+    this.tail = (this.tail !== llitem) ? this.tail : undefined;
+
+    this.count = this.count - (llitem ? 1 : 0);
+    return llitem?.item;
+  }
+
+  public empty(): boolean { return this.count === 0; }
 }

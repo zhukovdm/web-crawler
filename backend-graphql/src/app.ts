48fe82a7d7ed7725fec2caaaab2731a27f4f47bb
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2023-07-19 21:59:01 +0200

    change env variables

--- backend-graphql/src/app.ts
+++ backend-graphql/src/app.ts
@@ -1,15 +1,18 @@
+require("dotenv").config();
+
 import express from "express";
 import { createHandler } from "graphql-http/lib/use/express";
 import { schema } from "./graphql/schema";
 import { ModelFactory } from "./models";
 
 const wapp = express();
+const GRAPHQL_PORT = parseInt(process.env.GRAPHQL_PORT!);
 
 wapp.all("/graphql", createHandler({
   schema: schema,
   context: { model: ModelFactory.getModel() }
 }));
 
-wapp.listen(4000, () => {
-  console.log(`backend-graphql is listening on port 4000.`);
+wapp.listen(GRAPHQL_PORT, () => {
+  console.log(`backend-graphql is listening on port ${GRAPHQL_PORT}.`);
 });

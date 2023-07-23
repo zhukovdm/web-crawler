687b03e122422f5f7bee83d7ba5d823cc61ceffd
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2023-07-23 22:19:29 +0200

    add style checker

--- frontend/src/services/endpoint.ts
+++ frontend/src/services/endpoint.ts
@@ -1,16 +1,16 @@
 const env = process.env;
 
-const BACKEND_ADDR = env.REACT_APP_BACKEND_ADDR!;
+const BACKEND_ADDR = env.REACT_APP_BACKEND_ADDR ?? "localhost";
 
 const OPENAPI_ADDR =
-  `http://${BACKEND_ADDR}:${env.REACT_APP_OPENAPI_PORT!}/api/v1`;
+  `http://${BACKEND_ADDR}:${env.REACT_APP_OPENAPI_PORT ?? "3000"}/api/v1`;
 
 export const OPENAPI_REC_ADDR = OPENAPI_ADDR + "/records";
 
 export const OPENAPI_EXE_ADDR = OPENAPI_ADDR + "/executions";
 
 export const GRAPHQL_ADDR =
-  `http://${BACKEND_ADDR}:${env.REACT_APP_GRAPHQL_PORT!}/graphql`;
+  `http://${BACKEND_ADDR}:${env.REACT_APP_GRAPHQL_PORT ?? "4000"}/graphql`;
 
 export function getErrorMessage(res: Response): string {
   return `${res.statusText} (status code ${res.status})`;
